c32c4978f21cccbd13fe9ba1e49b832de21d47f5
Robbie Plankenhorn <rplankenhorn@gmail.com>
2015-03-09 19:23:31 -0500

    Part 1: Updated MainViewControllerHeaderView.

--- FirebaseSampleApp/FirebaseSampleApp/Views/MainViewControllerHeaderView.m
+++ FirebaseSampleApp/FirebaseSampleApp/Views/MainViewControllerHeaderView.m
@@ -9,10 +9,11 @@
 #import "MainViewControllerHeaderView.h"
 #import "UIView+AutoLayout.h"
 
+static CGSize const kHeaderButtonSize = {100.0, 33.0};
 static CGSize const kConnectionImageSize = {25.0, 25.0};
 
 @interface MainViewControllerHeaderView ()
-@property (strong, nonatomic) UILabel *headerLabel;
+@property (strong, nonatomic) UIButton *headerButton;
 @property (strong, nonatomic) UIImageView *connectionIndicatorImage;
 @end
 
@@ -20,40 +21,41 @@ static CGSize const kConnectionImageSize = {25.0, 25.0};
 
 #pragma mark - Properties
 
-- (UILabel *)headerLabel {
-    if (!_headerLabel) {
-        _headerLabel = [[UILabel alloc] init];
-        _headerLabel.font = [UIFont fontWithName:@"HelveticaNeue" size:20.0];
-        _headerLabel.textAlignment = NSTextAlignmentCenter;
-        _headerLabel.textColor = [UIColor whiteColor];
-        _headerLabel.backgroundColor = [UIColor colorWithRed:91.0/0xff green:149.0/0xff blue:214.0/0xff alpha:1.0];
-        _headerLabel.translatesAutoresizingMaskIntoConstraints = NO;
+- (UIButton *)headerButton {
+    if (!_headerButton) {
+        _headerButton = [[UIButton alloc] init];
+        _headerButton.titleLabel.font = [UIFont fontWithName:@"HelveticaNeue" size:20.0];
+        _headerButton.titleLabel.textAlignment = NSTextAlignmentCenter;
+        _headerButton.titleLabel.textColor = [UIColor whiteColor];
+        _headerButton.backgroundColor = [UIColor colorWithRed:91.0/0xff green:149.0/0xff blue:214.0/0xff alpha:1.0];
+        _headerButton.translatesAutoresizingMaskIntoConstraints = NO;
     }
-    return _headerLabel;
+    return _headerButton;
 }
 
 - (UIImageView *)connectionIndicatorImage {
     if (!_connectionIndicatorImage) {
         _connectionIndicatorImage = [[UIImageView alloc] init];
-        _connectionIndicatorImage.backgroundColor = [UIColor purpleColor];
+        _connectionIndicatorImage.image = [UIImage imageNamed:@"happy_face"];
+        _connectionIndicatorImage.contentMode = UIViewContentModeScaleAspectFit;
         _connectionIndicatorImage.translatesAutoresizingMaskIntoConstraints = NO;
     }
     return _connectionIndicatorImage;
 }
 
 - (NSString *)headerText {
-    return self.headerLabel.text;
+    return self.headerButton.titleLabel.text;
 }
 
 - (void)setHeaderText:(NSString *)headerText {
-    self.headerLabel.text = headerText;
+    [self.headerButton setTitle:headerText forState:UIControlStateNormal];
 }
 
 - (void)setConnectionActive:(BOOL)connectionActive {
     if (connectionActive) {
-        self.connectionIndicatorImage.backgroundColor = [UIColor greenColor];
+        self.connectionIndicatorImage.image = [UIImage imageNamed:@"happy_face"];
     } else {
-        self.connectionIndicatorImage.backgroundColor = [UIColor redColor];
+        self.connectionIndicatorImage.image = [UIImage imageNamed:@"sad_face"];
     }
 }
 
@@ -91,18 +93,18 @@ static CGSize const kConnectionImageSize = {25.0, 25.0};
     
     self.backgroundColor = [UIColor clearColor];
     
-    [self addSubview:self.headerLabel];
+    [self addSubview:self.headerButton];
     [self addSubview:self.connectionIndicatorImage];
     
-    NSDictionary *viewsDictionary = @{@"headerLabel": self.headerLabel,
+    NSDictionary *viewsDictionary = @{@"headerButton": self.headerButton,
                                       @"image": self.connectionIndicatorImage};
     
     NSDictionary *metrics = @{@"padding": @(10)};
     
-    [self.headerLabel constrainToSize:CGSizeMake(100.0, 33.0)];
+    [self.headerButton constrainToSize:kHeaderButtonSize];
     [self.connectionIndicatorImage constrainToSize:kConnectionImageSize];
     
-    [self.headerLabel centerInView:self];
+    [self.headerButton centerInView:self];
     
     [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:[image]-padding-|" options:0 metrics:metrics views:viewsDictionary]];
     [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-padding-[image]" options:0 metrics:metrics views:viewsDictionary]];
@@ -111,11 +113,11 @@ static CGSize const kConnectionImageSize = {25.0, 25.0};
 - (void)drawRect:(CGRect)rect {
     [super drawRect:rect];
     
-    UIBezierPath *maskPath = [UIBezierPath bezierPathWithRoundedRect:self.headerLabel.bounds byRoundingCorners:UIRectCornerAllCorners cornerRadii:CGSizeMake(7.0, 7.0)];
+    UIBezierPath *maskPath = [UIBezierPath bezierPathWithRoundedRect:self.headerButton.bounds byRoundingCorners:UIRectCornerAllCorners cornerRadii:CGSizeMake(7.0, 7.0)];
     CAShapeLayer *maskLayer = [CAShapeLayer layer];
-    maskLayer.frame = self.headerLabel.bounds;
+    maskLayer.frame = self.headerButton.bounds;
     maskLayer.path = maskPath.CGPath;
-    self.headerLabel.layer.mask = maskLayer;
+    self.headerButton.layer.mask = maskLayer;
 }
 
 @end
